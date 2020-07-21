e8e68efef70bdfd218836bac636500e86ca1df7b
leandro mara <leandrodavidm@hotmail.com>
2020-07-21 20:20:20 -0300

    se agregaron los ejercicios 3.1 3.2 3.3

--- bootcampgl2020/src/main/java/com/globallogic/app/App.java
+++ bootcampgl2020/src/main/java/com/globallogic/app/App.java
@@ -1,5 +1,8 @@
 package com.globallogic.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,15 +11,22 @@ public class App
 {
     public static void main( String[] args )
     {
-
+    	System.out.println("Ejercicio 3.1 de datos primitivos ");
         TiposPrimitivos variableNum= new TiposPrimitivos();
         variableNum.main();
+        System.out.println("Ejercicio 3.2 de primer variable ");
+        PrimeraVariable variable= new PrimeraVariable();
+        variable.main();
+        System.out.println("Ejercicio 3.2 de primer variable ");
+        PrimeraLista primerLista= new PrimeraLista();
+        primerLista.main();
     }
 }
 
 class TiposPrimitivos{
+	
 	//Metodo para prueba de tipos de variables
-	void main(){
+	public void main(){
 		int myNum =15;
 		char myChar='a';
 		boolean myBoolean= true;
@@ -31,9 +41,11 @@ class TiposPrimitivos{
 	}
 }
 
+
+
 class PrimeraVariable{
 	// Metodo para prueba de variable del tipo String
-	void main() {
+	public void main() {
 		String mensaje;
 		mensaje= "Valor inicial";
 		System.out.println( mensaje );
@@ -42,3 +54,24 @@ class PrimeraVariable{
 	}
 }
 
+class PrimeraLista{
+	//Metodo para pruebas de listas
+	public void main() {
+		List<String> letters = new ArrayList<String>();	
+		letters.add("A");
+		letters.add("B");
+		letters.add("C");
+		for(String letter: letters) {
+			System.out.println(letter);
+		}
+		
+		System.out.println("La lista contiene "+letters.size()+ " elementos");
+		
+		letters.add(0,"X");
+		
+		for(String letter: letters) {
+			System.out.println(letter);
+		}
+	}
+	
+}
